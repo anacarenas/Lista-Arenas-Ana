5efab81f3329880d8b73b8c424c4679df18cb915
anacarenas <anacarenasr@gmail.com>
2022-06-16 08:45:52 -0500

    Commit faltante

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Lista-Arenas-Ana';
+  show=true;
+  
+  listaAlumnos: any =[
+    {nombre: "Jose Ramirez", codigo: "20220001", estado: "Aprobado"},
+    {nombre: "Maria Guzman", codigo: "20220002", estado: "Aprobado"},
+    {nombre: "Juana Rojas", codigo: "20220003", estado: "Desaprobado"},
+    {nombre: "Katherine Ponce", codigo: "20220004", estado: "Aprobado"},
+    {nombre: "Cesar Rodríguez", codigo: "20220005", estado: "Desaprobado"}
+  ]
+
+  showCode():void{
+    this.show=!this.show;
+  }
 }
